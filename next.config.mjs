ba3cb1e28e34d9e522c83521702b97ed3ff7153f
ManouBlog <adjobikadjopierre27@gmail.com>
2025-05-27 17:31:43 +0000

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     reactStrictMode: true,
+  images: {
+    unoptimized: true, // Désactive l'optimisation si vous utilisez `next export`
+  },
+};
 
 export default nextConfig;
