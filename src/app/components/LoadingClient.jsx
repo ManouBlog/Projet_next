f04b0ec79fbae741f300c47315dbbdccb8d114f1
ManouBlog <adjobikadjopierre27@gmail.com>
2025-05-28 09:48:14 +0000

    new

--- src/app/components/LoadingClient.jsx
+++ src/app/components/LoadingClient.jsx
@@ -8,7 +8,9 @@ export default function LoadingClient() {
   return (
     isLoading ? <div className='fixed flex justify-center place-items-center
      place-content-center w-full h-full loading_client z-999999999'>
-      <h1 className='font-bold text-white'>Chargement...</h1>
+      {/* <h1 className='font-bold text-white'>Chargement...</h1> */}
+      <h1 className='font-semibold text-2xl text-white'>CHOISSIS
+        TON PRO...</h1>
     </div>:<div></div>
   )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <StoreProvider>
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
