4465b1ca9b2aae8d7aeeef39773bcf5332573c3c
ManouBlog <adjobikadjopierre27@gmail.com>
2025-05-28 13:26:08 +0000

    new

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -4,10 +4,10 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const tokenAwait = await cookies();
   const token = tokenAwait.get('authToken')?.value;
+  console.log("TOKEN EXIST",token)
   if(token){
-console.log("MYTOKEN",token)
+  console.log("MYTOKEN",token)
   const user = verifyToken(token)
-
   return NextResponse.json(user);
   }else{
     return NextResponse.json({status:401});
