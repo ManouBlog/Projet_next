58a5caaa65fff0513f5a5451eb8aa40cde5b3700
ManouBlog <adjobikadjopierre27@gmail.com>
2025-06-24 12:56:37 +0000

    new

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,12 +3,25 @@ import MyCard from '../components/dashboard/MyCard'
 
 function DashboardPage() {
     return (
+        <>
+         <div className='flex gap-10 my-10'>
+            <div>
+                <p>Revenu du mois</p>
+            <h1 className='text-xl font-semibold'>35000 Fcfa</h1>
+            </div>
+            <div>
+                <p>Profit du mois</p>
+            <h1 className='text-xl font-semibold'>35000 Fcfa</h1>
+            </div>
+         </div>
         <div className='flex gap-3 items-center flex-wrap justify-center'>
         <MyCard title='Total Clients' number='20'/>
         <MyCard title='Total Services' number='5'/>
         <MyCard title='Total Employés' number='2' />
         <MyCard title='Rendez-vous' number='2'/>
         </div>
+        
+        </>
     )
 }
 
