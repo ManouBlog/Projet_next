6b708366feac99c28acd2dfbaecab600ffa4296d
Github's AdjobiPierre <86586233+ManouBlog@users.noreply.github.com>
2025-07-31 07:36:08 +0000

    Update auth.js
    ajouts token

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -1,12 +1,15 @@
 import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers'
 
 const SECRET_KEY = process.env.NEXT_PUBLIC_JWT_SECRET;
 
 export function generateToken(userId) {
   return jwt.sign({ userId }, SECRET_KEY, { expiresIn: '100h' });
 }
-export function getTokenCookie(){
-return 'fjhfv';
+export async function getTokenCookie(){
+const tokenAwait = await cookies();
+  const token = tokenAwait.get('authToken')?.value;
+  return token;
 }
 
 export function verifyToken(token) {
