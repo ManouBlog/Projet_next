30b1d303eda7b1de364ccbdc6e534dde018d3a79
Github's AdjobiPierre <86586233+ManouBlog@users.noreply.github.com>
2025-07-31 07:34:13 +0000

    Update auth.js

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -5,6 +5,9 @@ const SECRET_KEY = process.env.NEXT_PUBLIC_JWT_SECRET;
 export function generateToken(userId) {
   return jwt.sign({ userId }, SECRET_KEY, { expiresIn: '100h' });
 }
+export function getTokenCookie(){
+return 'fjhfv';
+}
 
 export function verifyToken(token) {
    console.log("VERIFTOKEN",token)
